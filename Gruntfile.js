f5dce880e5f66800a82b6bfa3efe802818c5f137
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-04 17:26:25 -0400

    working skipping install extension json

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,26 +5,27 @@
  * Copyright (c) 2014 Yaw Joseph Etse. All rights reserved.
  */
 'use strict';
+var path = require('path');
 
 module.exports = function(grunt) {
   grunt.initConfig({
     jsbeautifier: {
-      files: ["<%= jshint.all %>"],
+      files: ['<%= jshint.all %>'],
       options: {
-        "indent_size": 2,
-        "indent_char": " ",
-        "indent_level": 0,
-        "indent_with_tabs": false,
-        "preserve_newlines": true,
-        "max_preserve_newlines": 10,
-        "brace_style": "collapse",
-        "keep_array_indentation": false,
-        "keep_function_indentation": false,
-        "space_before_conditional": true,
-        "eval_code": false,
-        "indent_case": false,
-        "unescape_strings": false,
-        "space_after_anon_function": true
+        'indent_size': 2,
+        'indent_char': ' ',
+        'indent_level': 0,
+        'indent_with_tabs': false,
+        'preserve_newlines': true,
+        'max_preserve_newlines': 10,
+        'brace_style': 'collapse',
+        'keep_array_indentation': false,
+        'keep_function_indentation': false,
+        'space_before_conditional': true,
+        'eval_code': false,
+        'indent_case': false,
+        'unescape_strings': false,
+        'space_after_anon_function': true
       }
     },
     simplemocha: {
@@ -62,14 +63,42 @@ module.exports = function(grunt) {
     },
     browserify: {
       dist: {
-        files: {
-          // 'example/test/slimscroll-index.js': ['client/test/scripts/slimscroll-index.js'],
-          // 'example/test/slimscroll-events.js': ['client/test/scripts/slimscroll-events.js'],
-          // 'build/main.js': ['client/scripts/**/*.js', 'client/scripts/**/*.coffee'],
-        },
-        options: {
-          // transform: ['coffeeify']
+        files: [{
+          expand: true,
+          cwd: 'resources',
+          src: ['**/*_src.js'],
+          dest: 'public',
+          rename: function (dest, src) {
+            var finallocation = path.join(dest, src);
+            finallocation = finallocation.replace('_src', '_build');
+            finallocation = finallocation.replace('resources', 'public');
+            finallocation = path.resolve(finallocation);
+            return finallocation;
+          }
+        }],
+        options: {}
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true,
+        compress: {
+          drop_console: false
         }
+      },
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: ['**/*_build.js'],
+          dest: 'public',
+          rename: function (dest, src) {
+            var finallocation = path.join(dest, src);
+            finallocation = finallocation.replace('_build', '.min');
+            finallocation = path.resolve(finallocation);
+            return finallocation;
+          }
+        }]
       }
     },
     watch: {
@@ -82,7 +111,7 @@ module.exports = function(grunt) {
           'resources/**/*.js',
           'test/**/*.js',
         ],
-        tasks: ['lint','browserify',/*'doc',*/ 'test'],
+        tasks: ['lint','browserify','jsbeautifier','uglify',/*'doc',*/ 'test'],
         options: {
           interrupt: true
         }
@@ -95,9 +124,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-uglify');
   grunt.loadNpmTasks('grunt-jsdoc');
 
-  grunt.registerTask('default', ['jshint', 'simplemocha']);
+  grunt.registerTask('default', ['jshint', 'jsbeautifier', 'simplemocha']);
   grunt.registerTask('lint', 'jshint');
   grunt.registerTask('doc','jsdoc');
   grunt.registerTask('test', 'simplemocha');
